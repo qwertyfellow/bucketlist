6bc75ed92314cbf55a3c30fe51cf4f642a98f6e5
Qwerty Fellow <chaitu.learnings.courses@gmail.com>
2025-06-26 23:52:27 -0700

    Adding user sign up functionality

--- auth.ts
+++ auth.ts
@@ -1,6 +1,9 @@
 // auth.ts (in project root or `src/`)
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { client } from "./sanity/lib/client";
+import { FETCH_USER_BY_GOOGLE_ID_QUERY } from "./sanity/queries/user";
+import { writeClient } from "./sanity/lib/writeClient";
 
 export const authOptions = {
   providers: [
@@ -10,13 +13,36 @@ export const authOptions = {
     }),
   ],
   callbacks: {
-    async signIn({user, account, profile}) {
-        console.log("############")
-        console.log(user)
-        console.log(account)
-        console.log(profile)
-        console.log("############")
-        return true
+    async signIn({ user, account, profile }) {
+      const googleId = account?.providerAccountId;
+      if (!googleId) {
+        console.error("No Google ID found");
+        return false;
+      }
+      const existingUser = await client
+      .withConfig({useCdn: false}) // USE THIS, else newly added users are not reflected immediately.
+      .fetch(FETCH_USER_BY_GOOGLE_ID_QUERY, {
+        id: googleId,
+      });
+      console.log("existingUser", existingUser)
+
+      if (!existingUser) {
+        await writeClient.create({
+          _type: "user",
+          authId: googleId,
+          name: user.name,
+          email: user.email,
+          image: user.image,
+          bio: "A cool bio...",
+          role: "user",
+        });
+        console.log("################################################");
+        console.log("User is just added.");
+        console.log("################################################");
+        return true;
+      }
+
+      return true;
     }
   }
 };
---
+++ sanity/queries/bucketlist.ts
@@ -0,0 +1,1 @@
+import { defineQuery } from "next-sanity";
