15c5a400f169addb457d13987eb8338f734d71ff
Qwerty Fellow <chaitu.learnings.courses@gmail.com>
2025-08-02 15:04:36 -0700

    Implemented creators profile page completely

--- app/(root)/creators/profile/[id]/page.tsx
+++ app/(root)/creators/profile/[id]/page.tsx
@@ -1,49 +1,105 @@
 import React from 'react'
-import { PageParams } from '@/constants/pages'
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
 import { auth } from '@/auth';
+import { client } from '@/sanity/lib/client';
+
+import { PageParams } from '@/constants/pages'
 import NotLoggedIn from '@/components/NotLoggedIn';
 import NotACreatorProfile from '@/components/NotACreatorProfile';
-import { client } from '@/sanity/lib/client';
-import { FETCH_CREATOR_BY_SANITY_ID_QUERY } from '@/sanity/queries/creator';
-import { notFound } from 'next/navigation';
 import NotAuthorised from '@/components/NotAuthorised';
+import BucketListCard from '@/components/BucketListItem';
 
-const Page = async  ({ params }: { params: Promise<PageParams> }) => {
+import { FETCH_CREATOR_BY_SANITY_ID_QUERY } from '@/sanity/queries/creator';
+import { FETCH_BUCKETLISTS_BY_CREATOR_ID } from '@/sanity/queries/bucketlist';
 
+const Page = async ({ params }: { params: Promise<PageParams> }) => {
   const id = (await params).id;
   const session = await auth();
 
-  const creator = await client.fetch(FETCH_CREATOR_BY_SANITY_ID_QUERY, {id: id})
+  const [creator, bucketListItems] = await Promise.all([
+    client.fetch(FETCH_CREATOR_BY_SANITY_ID_QUERY, { id }),
+    client.fetch(FETCH_BUCKETLISTS_BY_CREATOR_ID, { id })
+  ])
 
-  // Handler when bucketlist not found use-case.
-  if(!creator) return notFound();
+  if (!creator) return notFound();
 
-  // Destructure the data
   const { bio, email, image, name } = creator;
-  const isAuthorised = session?.user?.sanityId === creator?._id
+  const isAuthorised = session?.user?.sanityId === creator?._id;
 
-  /**
-   * 4. Handle success case
-   * 5. Link the profile page on the navbar
-   */
+  const renderBucketListItems = (items: any[]) => {
+    if (items?.length === 0) {
+      return <>
+        <h1 className="sub-heading text-secondary">No bucketlists found</h1>
+        <p className="text-14-normal mb-5">Please create new bucketlists</p>
+      </>
+    }
+
+    return <>
+    <p className="text-14-normal mb-5">You can edit, make live, delete the below items.</p>
+    <div className="card_grid">
+      {items.map((item: any) => (
+        <BucketListCard
+        key={item._id}
+        id={item._id}
+        title={item.title}
+        destination={item.destination}
+        category={item.category}
+        likes={item.likes}
+        creatorName={item.creator.name}
+        creatorImage={item.creatorImage}
+        slug={item.slug?.current}
+        />
+      ))}
+    </div>
+    </>
+  };
 
   const renderView = () => {
     if (!session) {
-      return <NotLoggedIn />
-    } else if (session && session?.user?.loginType != "creator") {
-      return <NotACreatorProfile />
-    } else if (session && session?.user?.loginType == "creator" && !isAuthorised) {
-      return <NotAuthorised />
+      return <NotLoggedIn />;
+    } else if (session?.user?.loginType !== 'creator') {
+      return <NotACreatorProfile />;
+    } else if (!isAuthorised) {
+      return <NotAuthorised />;
     }
-    return <div>
-        <h1>Creators profile page {id}</h1>
-    </div>
-  }
+
     return (
       <>
-        {renderView()}
+        <div className="showcase bg-primary">
+          <main className="section_container">
+            <div className="flex items-center gap-4 mb-4">
+              {image && (
+                <Image
+                  src={image}
+                  alt={`${name}'s avatar`}
+                  width={64}
+                  height={64}
+                  className="rounded-full border border-white"
+                />
+              )}
+              <h1 className="heading text-secondary">Welcome, {name}</h1>
+            </div>
+            {/* <p className="text-30-semibold text-white mb-5">{bio}</p> */}
+            <p className="text-30-semibold text-white mb-5">
+              This is the place where you can find all details related to your profile/account.
+            </p>
+          </main>
+        </div>
+        <div className="section_container">
+          <h1 className="text-30-bold mb-2">Your bucketlists</h1>
+          {renderBucketListItems(bucketListItems)}
+        </div>
       </>
-    )
-}
+    );
+  };
 
-export default Page
+  return <>{renderView()}</>;
+};
+
+export default Page;
+
+/**
+ * 1. Add Create CTA to Create a new item
+ * 2. Add Edit and Delete buttons for each item
+ */
