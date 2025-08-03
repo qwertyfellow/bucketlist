8d5d78796c0e9e8c620fe6ed0e7e88cf828f7f37
Qwerty Fellow <chaitu.learnings.courses@gmail.com>
2025-08-03 00:06:37 -0700

    Adding delete server action

---
+++ lib/actions/bucketlist/deleteBucketList.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { auth } from "@/auth";
+import { parseServerActionResponse } from "@/lib/utils";
+import { writeClient } from "@/sanity/lib/writeClient";
+import "server-only";
+
+const deleteBucketListAction = async (bucketListId: string) => {
+
+    // 1. Verify session details
+    const session = await auth();
+    if (!session) {
+        return parseServerActionResponse({
+        status: "ERROR",
+        error: "You must be logged in to delete a bucketlist.",
+        });
+    }
+
+    try {
+        // 2. Use write client to delete the item from Sanity
+        const response = await writeClient.delete(bucketListId);
+
+        // 3. Successful response
+        return parseServerActionResponse({
+            ...response,
+            error: "",
+            status: "SUCCESS",
+        });
+    } catch (error) {
+        // 4. Error response
+        return parseServerActionResponse({
+            status: "ERROR",
+            error: JSON.stringify(error),
+        });
+    }
+};
+
+export default deleteBucketListAction;
