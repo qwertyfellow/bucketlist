fe05cb46c28bf1247030b895d5edf6101ac477d1
Qwerty Fellow <chaitu.learnings.courses@gmail.com>
2025-08-06 21:59:54 -0700

    Adding avatar for logged in user and linking the profile page in the menu

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,13 @@
 import { auth } from "@/auth";
 import { loginHandler, logoutHandler } from "@/lib/actions/auth/authentication";
 import Link from "next/link";
+import AvatarMenu from "./AvatarMenu";
 
 export default async function Navbar() {
   const session = await auth();
 
   return (
-    <nav className="w-full ps-2">
+    <nav className="w-full px-4 py-2 border-b">
       <div className="flex items-center justify-between">
         {/* Logo */}
         <Link href="/" className="text-xl font-bold text-black">
@@ -14,29 +15,17 @@ export default async function Navbar() {
         </Link>
 
         {/* Nav items */}
-        <ul className="flex space-x-4 items-center">
-          <li>
-            {session? (
-              <form action={logoutHandler}>
-                <button
-                  type="submit"
-                  className="bg-secondary rounded px-3 py-2 m-2 text-white"
-                >
-                  Log out
-                </button>
-              </form>
-            ) : (
-              <form action={loginHandler}>
-                <button
-                  type="submit"
-                  className="bg-secondary rounded px-3 py-2 m-2 text-white"
-                >
-                  Login
-                </button>
-              </form>
-            )}
-          </li>
-        </ul>
+        <div className="flex items-center space-x-4">
+          {session?.user && <AvatarMenu user={session.user} />}
+          <form action={session ? logoutHandler : loginHandler}>
+            <button
+              type="submit"
+              className="bg-secondary rounded px-3 py-2 text-white"
+            >
+              {session ? "Log out" : "Login"}
+            </button>
+          </form>
+        </div>
       </div>
     </nav>
   );
